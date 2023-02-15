b8df5ff955e1280b62cb4356a7f9c792e151d236
Venusatya Mallidi <venumallidi1999@gmail.com>
2023-02-15 15:46:07 +0530

    Added the Rock Band Generator Program

---
+++ Rock Band Generator/main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator!")
+# Taking the city as input from user
+city = input("Enter the city where you grew?\n")
+# Taking name as input from user
+name = input("Enter the name of your pet\n")
+#Rock Band name is the combination of city and name
+print("The name of your Rock Band Could be " + city +" " +name)
